0fd1296139b9a8c992cbbdfa8a48d7817b5601b4
Sebastian Pipping <sebastian@pipping.org>
2018-11-03 01:26:23 +0100

    setup.py: Drop support for Python 2.5 and 2.6

--- setup.py
+++ setup.py
@@ -27,8 +27,6 @@ setup(
         'License :: OSI Approved :: GNU General Public License (GPL)',
         'Natural Language :: English',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.5',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
     ],
